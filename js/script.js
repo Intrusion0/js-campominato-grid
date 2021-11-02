a56514eecb96fd0dc8dcedf934c49c3560bf57ae
Mario <lombardomario01@gmail.com>
2021-11-02 15:25:20 +0100

    logical structure done

--- js/script.js
+++ js/script.js
@@ -11,7 +11,15 @@ Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro (
 
 /*
 
-1. 
+1. Genero la griglia di gioco con le varie celle in base alla difficoltà selezionata => for
+    1.2 Se seleziona "Facile" (difficoltà 1), eseguirò un ciclo for che si ripeterà 100 volte, visto che dovrò stampare 100 celle. => for i < 100
+    1.2 Se seleziona "Medio" (difficoltà 2), eseguirò un ciclo for che si ripeterà 81 volte, visto che dovrò stampare 81 celle. => for i < 81
+    1.3 Se seleziona "Difficile" (difficoltà 3), eseguirò un ciclo for che si ripeterà 49 volte, visto che dovrò stampare 49 celle. => for i < 49
+2. Creo un evento al click della cella, in modo che si colora quando viene selezionata
+    2.1 Creo una funzione all'interno del ciclo => function squareActive()
+        2.1.1 Creo un elemento => "div"
+        2.1.2 Aggiungo una classe all'elemento creato => classList.add("square")
+        2.1.3 Appendo l'elemento con la classe alla variabile che fa riferimento all'id di un tag html => .append 
 
 */
 
